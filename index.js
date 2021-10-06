d74ee597c708f255afa4cde857cea105ea55d4d3
Shane Moyo <shane.moyo@ifit.com>
2021-10-06 14:31:50 -0700

    wip

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
     if (req.method == 'GET') {
